c17121f37343b40145dbec16b981f09ab5dda13c
ofuleme <bencosoft@gmail.com>
2022-10-24 11:06:44 -0700

    Love

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,36 +1,42 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * _atoi - convers a string to an int
- * @s: a pointer to a string guven by main
- *
- * Description: making our own atoi function
- * Return: returns 0 if no numbers and the result if numbers
+ * _atoi - Convert a string to an integer.
+ * @s: char array string
+ * Description: Number in the string can be preceded by an infinite
+ * number of characters.
+ * You need to take into account all -/+ signs before the number.
+ * If there are no numbers in the string, return 0.
+ * No need to check for overflow.
+ * Not allowed to hard-code special values.
+ * Return: first integer found in string
  */
 
 int _atoi(char *s)
 {
-	int counter = 0;/* A */
-	int i = 0;
-	unsigned int result = 0;
-	int sign = 0;
+	int i;
+	int h, p;
 
-	while (s[counter] != '\0')/* B */
+	h = 0;
+	p = -1;
+	for (i = 0; s[i] != '\0'; i++)
 	{
-		if (s[counter] == '-')/* C */
-			sign++;
-		if (s[counter] >= 48 && s[counter] <= 57)/* D */
+		if (s[i] == '-')
+			p *= -1;
+
+		if (s[i] > 47 && s[i] < 58)
 		{
-			i++;
-			result = result * 10 + s[counter] - '0';/* E */
+			if (h < 0)
+				h = (h * 10) - (s[i] - '0');
+			else
+				h = (s[i] - '0') * -1;
+
+			if (s[i + 1] < 48 || s[i + 1] > 57)
+				break;
 		}
-		if (i > 0 && (s[counter] < 48 || s[counter] > 57))/* F */
-			break;
-		counter++;/* G */
 	}
-	if (i == 0)/* H */
-		return (0);
-	if (sign % 2 == 0)/* I */
-		return (result);
-	return (result * -1);/* J */
+	if (p < 0)
+		h *= -1;
+
+	return (h);
 }
