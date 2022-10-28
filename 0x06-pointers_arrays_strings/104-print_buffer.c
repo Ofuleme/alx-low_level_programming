63288b8d7e775fddd114ba042dcf879985813f3e
ofuleme <bencosoft@gmail.com>
2022-10-28 14:02:18 -0700

    Love

--- 0x06-pointers_arrays_strings/104-print_buffer.c
+++ 0x06-pointers_arrays_strings/104-print_buffer.c
@@ -46,8 +46,8 @@ void print_buffer(char *b, int size)
 
 	for (i = 0; i <= (size - 1) / 10 && size; i++)
 	{
-	printf("%08x: ", i * 10);
-	if (i < size / 10)
+		printf("%08x: ", i * 10);
+		if (i < size / 10)
 		{
 			print_line(b, 9, i);
 		}
