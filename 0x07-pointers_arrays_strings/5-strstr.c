e6d6da71461d9f39f18af3a8cb5dc31026e2940c
ofuleme <bencosoft@gmail.com>
2022-11-01 10:18:07 -0700

    Love

--- 0x07-pointers_arrays_strings/5-strstr.c
+++ 0x07-pointers_arrays_strings/5-strstr.c
@@ -1,32 +1,34 @@
 #include "main.h"
 
 /**
- * _strstr - function locate
- * @haystack: pointer to char
- * @needle: pointer to char
- * Return: 0
+ * _strstr - locates a substring
+ *
+ * @haystack: the longer string to search
+ * @needle: the first occurrence of the substring
+ *
+ * Return: a pointer beg of substring or @Null if it not found.
  */
 
 char *_strstr(char *haystack, char *needle)
 {
-	char *result = haystack, *fneedle = needle;
+	unsigned int i = 0, j = 0;
 
-	while (*haystack)
+	while (haystack[i])
 	{
-		while (*needle)
+		while (needle[j] && (haystack[i] == needle[0]))
 		{
-			if (*haystack++ != *needle++)
-			{
+			if (haystack[i + j] == needle[j])
+				j++;
+			else
 				break;
-			}
 		}
-		if (!*needle)
+		if (needle[j])
 		{
-	return (result);
-}
-needle = fneedle;
-result++;
-haystack = result;
-}
-return (0);
+			i++;
+			j = 0;
+		}
+		else
+			return (haystack + i);
+	}
+	return (0);
 }
